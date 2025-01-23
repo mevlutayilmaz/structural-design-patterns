3c91c72ababe007c7734ceb0cdb3122dc95a5918
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2025-01-23 23:48:51 +0300

    adapter design pattern example

---
+++ AdapterDesignPattern/AdapterDesignPattern.Example1/Program.cs
@@ -0,0 +1,32 @@
+﻿Adaptee adaptee = new();
+ITarget target = new Adapter(adaptee);
+
+Console.WriteLine(target.GetRequest());
+
+public interface ITarget
+{
+    string GetRequest();
+}
+
+class Adaptee
+{
+    public string GetSpecificRequest()
+    {
+        return "Specific request.";
+    }
+}
+
+class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public string GetRequest()
+    {
+        return $"This is '{_adaptee.GetSpecificRequest()}'";
+    }
+}
