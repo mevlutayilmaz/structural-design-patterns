e1ba690bcb099614c777a2fe50c10cca9ac39c24
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2025-02-06 23:07:44 +0300

    proxy design pattern example

---
+++ ProxyDesignPattern/ProxyDesignPattern.Example1/Program.cs
@@ -0,0 +1,49 @@
+﻿
+Console.WriteLine("Client: Executing the client code with a real subject:");
+RealSubject realSubject = new();
+realSubject.Request();
+
+Console.WriteLine();
+
+Console.WriteLine("Client: Executing the same client code with a proxy:");
+Proxy proxy = new(realSubject);
+proxy.Request();
+
+public interface ISubject
+{
+    void Request();
+}
+
+public class RealSubject : ISubject
+{
+    public void Request()
+        => Console.WriteLine("RealSubject: Handling Request.");
+}
+
+public class Proxy : ISubject
+{
+    private RealSubject _realSubject;
+
+    public Proxy(RealSubject realSubject)
+    {
+        _realSubject = realSubject;
+    }
+
+    public void Request()
+    {
+        if (CheckAccess())
+        {
+            _realSubject.Request();
+            LogAccess();
+        }
+    }
+
+    public bool CheckAccess()
+    {
+        Console.WriteLine("Proxy: Checking access prior to firing a real request.");
+        return true;
+    }
+
+    public void LogAccess()
+        => Console.WriteLine("Proxy: Logging the time of request.");
+}
